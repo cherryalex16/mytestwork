da16423fceded492e0f56dbe5393e7aab096f8ea
cherryalex16 <yaarendler@gmail.com>
2013-08-04 23:16:53 -0400

    add private

--- oop.php
+++ oop.php
@@ -1,9 +1,21 @@
 <?php
  class hello {
-   function __construct($stuff) {
-     echo $stuff . '/n';
+
+//    public $name;
+    private $ages;
+  function setName() {
+    $ages = $age;
    }
- }
+   function __construct($stuff) {
+   echo $stuff;
+   }
+  // $this->ages = $age;
+}
   $obj = new hello('hello world pcs');
   $obj = new hello('asher');
+  $obj = new hello('ashe rendler');
+  $age = new hello ('32');
+  
+
+
 ?>
