333e9bd87f5306ff31a01bb1ff37e6774d785d2f
cherryalex16 <yaarendler@gmail.com>
2013-08-05 13:20:05 -0400

    made class

---
+++ oreilly.php
@@ -0,0 +1,17 @@
+<?
+  $object = new user;
+  print_r($object); 
+  class user{
+    public $name, $password;
+      function save_user()
+    {
+        echo "save user code goes here";
+    }  
+  } 
+ 
+
+
+
+
+   
+?>
