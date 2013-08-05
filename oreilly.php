98593990f0ffe3cf8482955bb33eea533e68fd30
cherryalex16 <yaarendler@gmail.com>
2013-08-05 13:35:52 -0400

    added methods

--- oreilly.php
+++ oreilly.php
@@ -1,6 +1,12 @@
 <?
   $object = new user;
-  print_r($object); 
+  print_r($object); echo "<br/>";
+  $object->name = "asher";
+  $object->password = "mypass";
+  print_r($object); echo "<br/>";
+  $object->save_user();
+  
+
   class user{
     public $name, $password;
       function save_user()
