d4f09b1d66d4d2d779a659d5e7ee76e29a3b0cc3
cherryalex16 <yaarendler@gmail.com>
2013-08-06 14:03:15 -0400

    foreach

--- array.php
+++ array.php
@@ -1,10 +1,21 @@
 <?
 
- $myarray = array('steve','joe','stan');
+// $myarray = array('steve','joe','stan')
 
- $associative_array = array('name1'=>'joe', 'name2' => 'stan');
- echo  $myarray[1] .    $myarray[2];
- 
+
+
+
+
+ $associative_array = array('name1'=>'joe', 'name2' => 'stan','name3' =>'asher');
+// echo  $myarray[1] .    $myarray[2];
+ $j = 0;
+ foreach ($associative_array as $item)
+ {
+  echo "$j: $item<br>";
+  ++$j;
+ }
+
+
  //echo count($associative_array);
  //echo count($myarray);
 
