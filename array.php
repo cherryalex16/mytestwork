8916f28b8466387a203485700268022ca9e9fe38
cherryalex16 <yaarendler@gmail.com>
2013-08-06 14:14:55 -0400

    foreach with echo

--- array.php
+++ array.php
@@ -9,9 +9,9 @@
  $associative_array = array('name1'=>'joe', 'name2' => 'stan','name3' =>'asher');
 // echo  $myarray[1] .    $myarray[2];
  $j = 0;
- foreach ($associative_array as $item)
+ foreach ($associative_array as $item => $description)
  {
-  echo "$j: $item<br>";
+  echo "$item: $description<br>";
   ++$j;
  }
 
