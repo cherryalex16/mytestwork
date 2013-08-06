8cfae8cda3161f947923813138a5ce7279ef3a39
cherryalex16 <yaarendler@gmail.com>
2013-08-06 14:50:33 -0400

    count

--- array.php
+++ array.php
@@ -8,16 +8,14 @@
 
  $associative_array = array('name1'=>'joe', 'name2' => 'stan','name3' =>'asher');
 // echo  $myarray[1] .    $myarray[2];
- $j = 0;
+ 
  foreach ($associative_array as $item => $description)
  {
-  echo "$item: $description<br>";
-  ++$j;
+  echo "$item: $description <br>";
+  
  }
 
-
- //echo count($associative_array);
- //echo count($myarray);
+ echo " count $item";
 
 
 
