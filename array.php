7020f572716eeea442ab34191d98ad41f6d2f5be
cherryalex16 <yaarendler@gmail.com>
2013-08-05 21:10:21 -0400

    practice array

---
+++ array.php
@@ -0,0 +1,15 @@
+<?
+//e.g regular array
+  $myarray = array('steve','joe','stan');
+
+  print_r($myarray);
+
+
+ $associative_array = array('name1'=>'joe', 'name2' => 'stan');
+ $associative_array[] = ('name4' => 'asher');
+
+//   print_r($associative_array);
+
+
+?>
+
