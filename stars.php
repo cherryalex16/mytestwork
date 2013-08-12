1e7d6fe91fc92cd431ff8afcee2e1e9fc2e2cf2c
cherryalex16 <yaarendler@gmail.com>
2013-08-12 19:30:35 -0400

    forloop

---
+++ stars.php
@@ -0,0 +1,12 @@
+<?php
+  for ($row = 1; $row < 11; $row++)
+    {
+         for ($stars = 0; $stars < $row; $stars++)
+           {
+             echo "*";
+           }                                                                                   echo "<br>";
+    }
+
+
+
+?>
