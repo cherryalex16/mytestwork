dee1e7aa2c3e832de7bd9f1c45e29eb92b3d3340
cherryalex16 <yaarendler@gmail.com>
2013-08-14 21:28:48 -0400

    class with function

--- oop.php
+++ oop.php
@@ -3,11 +3,15 @@
   {
     public $age;
     public $name;
+      function  speak()
+       {
+          echo "hello my name is: " . $this->name;
+       }
   }
   $me = new Person();
   $me->age = 35;
   $me->name = 'Asher';
-
+  $me->speak();
   print_r ($me)
   
 
